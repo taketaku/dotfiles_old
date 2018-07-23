af682ffd4a656508b5351f3581320c13f44ea2bf
taketaku <vbvbsp48@gmail.com>
2018-07-23 17:00:36 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## 使い方
+1. 
+ホームティレクトリ下にdotfilesをclone
+2. 
+> $ cd dotfiles
+3.
+> $ ./dotfilesLink.sh
---
+++ dotfilesLink.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+ln -fns ~/dotfiles/.bashrc ~/.bashrc
+ln -fns ~/dotfiles/.bash_aliases ~/.bash_aliases
+ln -fns ~/dotfiles/.gitconfig ~/.gitconfig
+ln -fns ~/dotfiles/.config/nvim ~/.config/nvim
