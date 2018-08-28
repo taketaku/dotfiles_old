bace724ce7f65248b4397a19bf61ae1fa6e8201e
taketaku <vbvbsp48@gmail.com>
2018-08-28 16:14:08 +0900

    tmuxの設定追加

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -4,3 +4,4 @@ ln -fns ~/dotfiles/.bash_aliases ~/.bash_aliases
 ln -fns ~/dotfiles/.bash_profile ~/.bash_profile
 ln -fns ~/dotfiles/.gitconfig ~/.gitconfig
 ln -fns ~/dotfiles/.config/nvim ~/.config/nvim
+ln -fns ~/dotfiles/.tmux.conf ~/.tmux.conf
