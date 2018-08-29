5cf68712a2377283e9baf1f406399b6469be474b
taketaku <vbvbsp48@gmail.com>
2018-08-29 15:26:33 +0900

    add VSC settings

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -4,4 +4,5 @@ ln -fns ~/dotfiles/.bash_aliases ~/.bash_aliases
 ln -fns ~/dotfiles/.bash_profile ~/.bash_profile
 ln -fns ~/dotfiles/.gitconfig ~/.gitconfig
 ln -fns ~/dotfiles/.config/nvim ~/.config/nvim
+ln -fns ~/dotfiles/.config/Code/User/settings.json ~/.config/Code/User/settings.json
 ln -fns ~/dotfiles/.tmux.conf ~/.tmux.conf
